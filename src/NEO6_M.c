b68ac7e712c829bbfc65192905f07814f6f4c3b6
rostyslav-sydor <rostyslav.sydor@ucu.edu.ua>
2022-01-13 20:51:04 +0200

    Refactored message parsing

--- src/NEO6_M.c
+++ src/NEO6_M.c
@@ -5,6 +5,7 @@ void NEOSetWriteCallback(int (*write)(uint8_t *buffer, int length)){
 }
 
 void NEOGetData(char* data, NEOData* gpsData){
+
     char* tmpData = strdup(data);
     int starPos = strchr(tmpData, '*') - tmpData;
     tmpData[starPos] = ',';
@@ -17,7 +18,7 @@ void NEOGetData(char* data, NEOData* gpsData){
 
         if(!(match - prevMatch - 1)) {
             prevMatch += 1;
-            values[i] = NULL;
+            values[i] = 0;
             i++;
             continue;
         }
@@ -34,82 +35,172 @@ void NEOGetData(char* data, NEOData* gpsData){
     free(value);
     char* errptr;
 
+    void* parameters[45] = {0};
+    Types types[45] = {0};
+    int paramCount = 0;
+
     if (strstr(values[0], "RMC")){
-        gpsData->UTCTime = atof(values[1]);
-        if(values[2]) gpsData->dataStatus = *values[2];
-        gpsData->latitude = atof(values[3]);
-        if(values[4]) gpsData->NS = *values[4];
-        gpsData->longitude = atof(values[5]);
-        if(values[6]) gpsData->EW = *values[6];
-        gpsData->speedKnots = atof(values[7]);
-        gpsData->courseMagnetic = atof(values[8]);
-        gpsData->date = strtol(values[9], &errptr, 10);
-        if(values[12]) gpsData->mode = *values[12];
+        void* tmpParameters[] = {&gpsData->UTCTime, &gpsData->dataStatus,
+                                 &gpsData->latitude, &gpsData->NS,
+                                 &gpsData->longitude, &gpsData->EW,
+                                 &gpsData->speedKnots, &gpsData->courseMagnetic,
+                                 &gpsData->date, NULL, NULL, &gpsData->mode};
+
+        Types tmpTypes[] = {Edouble, Echar,
+                            Edouble, Echar,
+                            Edouble, Echar,
+                            Edouble, Edouble,
+                            Eint, Enull, Enull, Echar};
+
+        paramCount = sizeof(tmpParameters)/sizeof(tmpParameters[0]);
+        memcpy(parameters, tmpParameters, paramCount*sizeof(void*));
+        memcpy(types, tmpTypes, paramCount*sizeof(Types));
     }
 
     else if(strstr(values[0], "GSV")){
-        gpsData->satellitesUsed = strtol(values[3], &errptr, 10);
+        int msgNum = strtol(values[2], &errptr, 10);
 
-        int msgIndex = (strtol(values[2], &errptr, 10) - 1) * 4;
-        int j;
-        for (j = 0; j< 4; j++){
-            gpsData->satelliteIDs[msgIndex+j] = strtol(values[4 + j*4], &errptr, 10);
-            gpsData->elevations[msgIndex+j] = strtol(values[5 + j*4], &errptr, 10);
-            gpsData->azimuths[msgIndex+j] = strtol(values[6 + j*4], &errptr, 10);
-            gpsData->SNR[msgIndex + j] = strtol(values[7 + j * 4], &errptr, 10);
-        }
+
+        void* tmpParameters[] = {&gpsData->satellitesUsed, &gpsData->satellitesUsed, // skip first 2 values
+                                 &gpsData->satellitesUsed,
+                                 &gpsData->satelliteIDs[(msgNum-1)*4], &gpsData->elevations[(msgNum-1)*4],
+                                 &gpsData->azimuths[(msgNum-1)*4], &gpsData->SNR[(msgNum-1)*4],
+                                 &gpsData->satelliteIDs[(msgNum-1)*4+1], &gpsData->elevations[(msgNum-1)*4+1],
+                                 &gpsData->azimuths[(msgNum-1)*4+1], &gpsData->SNR[(msgNum-1)*4+1],
+                                 &gpsData->satelliteIDs[(msgNum-1)*4+2], &gpsData->elevations[(msgNum-1)*4+2],
+                                 &gpsData->azimuths[(msgNum-1)*4+2], &gpsData->SNR[(msgNum-1)*4+2],
+                                 &gpsData->satelliteIDs[(msgNum-1)*4+3], &gpsData->elevations[(msgNum-1)*4+3],
+                                 &gpsData->azimuths[(msgNum-1)*4+3], &gpsData->SNR[(msgNum-1)*4+3]};
+
+        Types tmpTypes[] = {Eint, Eint,
+                            Eint,
+                            Eint, Eint, Eint, Eint,
+                            Eint, Eint, Eint, Eint,
+                            Eint, Eint, Eint, Eint,
+                            Eint, Eint, Eint, Eint};
+
+        paramCount = sizeof(tmpParameters)/sizeof(tmpParameters[0]);
+        memcpy(parameters, tmpParameters, paramCount*sizeof(void*));
+        memcpy(types, tmpTypes, paramCount*sizeof(Types));
     }
 
     else if(strstr(values[0], "GGA")){
-        gpsData->UTCTime = atof(values[1]);
-        gpsData->latitude = atof(values[2]);
-        if(values[3]) gpsData->NS = *values[3];
-        gpsData->longitude = atof(values[4]);
-        if(values[5]) gpsData->EW = *values[5];
-        gpsData->positionFixIndicator = strtol(values[6], &errptr, 10);
-        gpsData->satellitesUsed = strtol(values[7], &errptr, 10);
-        gpsData->HDOP = atof(values[8]);
-        gpsData->MSLAltitude = atof(values[9]);
-        gpsData->geoidSeparation = atof(values[10]);
-        gpsData->DGPSStationID = strtol(values[11], &errptr, 10);
+        void* tmpParameters[] = {&gpsData->UTCTime,
+                                 &gpsData->latitude, &gpsData->NS,
+                                 &gpsData->longitude, &gpsData->EW,
+                                 &gpsData->positionFixIndicator, &gpsData->satellitesUsed,
+                                 &gpsData->HDOP, &gpsData->MSLAltitude, &gpsData->geoidSeparation,
+                                 &gpsData->DGPSStationID};
+
+        Types tmpTypes[] = {Edouble,
+                            Edouble, Echar,
+                            Edouble, Echar,
+                            Eint, Eint,
+                            Edouble, Edouble, Edouble,
+                            Eint};
+
+        paramCount = sizeof(tmpParameters)/sizeof(tmpParameters[0]);
+        memcpy(parameters, tmpParameters, paramCount*sizeof(void*));
+        memcpy(types, tmpTypes, paramCount*sizeof(Types));
+
     }
 
     else if(strstr(values[0], "GSA")){
-        gpsData->PDOP = atof(values[15]);
-        gpsData->HDOP = atof(values[16]);
-        gpsData->VDOP = atof(values[17]);
+        void* tmpParameters[] = {&gpsData->mode, &gpsData->positionFixIndicator,
+                                 &gpsData->satelliteIDs[0], &gpsData->satelliteIDs[1],
+                                 &gpsData->satelliteIDs[2], &gpsData->satelliteIDs[3],
+                                 &gpsData->satelliteIDs[4], &gpsData->satelliteIDs[5],
+                                 &gpsData->satelliteIDs[6], &gpsData->satelliteIDs[7],
+                                 &gpsData->satelliteIDs[8], &gpsData->satelliteIDs[9],
+                                 &gpsData->satelliteIDs[10], &gpsData->satelliteIDs[11],
+                                 &gpsData->PDOP, &gpsData->HDOP, &gpsData->VDOP};
+
+        Types tmpTypes[] = {Echar, Eint,
+                            Eint, Eint, Eint, Eint, Eint, Eint,
+                            Eint, Eint, Eint, Eint, Eint, Eint,
+                            Edouble, Edouble, Edouble};
+
+        paramCount = sizeof(tmpParameters)/sizeof(tmpParameters[0]);
+        memcpy(parameters, tmpParameters, paramCount*sizeof(void*));
+        memcpy(types, tmpTypes, paramCount*sizeof(Types));
+
     }
 
     else if(strstr(values[0], "VTG")){
-        gpsData->courseTrue = atof(values[1]);
-        gpsData->courseMagnetic = atof(values[2]);
-        gpsData->speedKnots = atof(values[3]);
-        gpsData->speedKpH = atof(values[4]);
-        if(values[5])gpsData->mode = *values[5];
+        void* tmpParameters[] = {&gpsData->courseTrue, &gpsData->mode,
+                                 &gpsData->courseMagnetic, &gpsData->mode,
+                                 &gpsData->speedKnots, &gpsData->speedKpH,
+                                 &gpsData->mode};
+
+        Types tmpTypes[] = {Edouble, Echar,
+                            Edouble, Echar,
+                            Edouble, Edouble,
+                            Echar};
+
+        paramCount = sizeof(tmpParameters)/sizeof(tmpParameters[0]);
+        memcpy(parameters, tmpParameters, paramCount*sizeof(void*));
+        memcpy(types, tmpTypes, paramCount*sizeof(Types));
+
     }
     else if(strstr(values[0], "ZDA")){
-        gpsData->UTCTime = atof(values[1]);
-        gpsData->UTCDay = strtol(values[2], &errptr, 10);
-        gpsData->UTCMonth = strtol(values[3], &errptr, 10);
-        gpsData->UTCYear = strtol(values[4], &errptr, 10);
-        gpsData->localHours = strtol(values[5], &errptr, 10);
-        gpsData->localMinutes = strtol(values[7], &errptr, 10);
+        void* tmpParameters[] = {&gpsData->UTCTime,
+                                 &gpsData->UTCDay, &gpsData->UTCMonth, &gpsData->UTCYear,
+                                 &gpsData->localHours, &gpsData->localMinutes};
+
+        Types tmpTypes[] = {Edouble,
+                            Eint, Eint, Eint,
+                            Eint, Eint};
+
+        paramCount = sizeof(tmpParameters)/sizeof(tmpParameters[0]);
+        memcpy(parameters, tmpParameters, paramCount*sizeof(void*));
+        memcpy(types, tmpTypes, paramCount*sizeof(Types));
+
     }
-}
+    
+    if(paramCount){
+        i = 0;
+        for(; i < paramCount; ++i){
+            switch (types[i]) {
+                case Eint: {
+                    int tmp = strtol(values[i+1], &errptr, 10);
+                    memcpy(parameters[i],&tmp, sizeof(int));
+                    break;
+                }
 
+                case Edouble: {
+                    double tmp = atof(values[i+1]);
+                    memcpy(parameters[i], &tmp, sizeof(double));
+                    break;
+                }
 
-void NEOMessageEnable(int msgClass, int msgId, int enable){
-    uint8_t msg[] = {0x06, 0x01, 0x03, 0x00, msgClass, msgId, enable};
-    NEOSendMessage(msg, 7);
+                case Echar: {
+                    char tmp = values[i+1][0];
+                    memcpy(parameters[i], &tmp, 1);
+                    break;
+                }
+
+                default:
+                    break;
+            }
+
+        }
+    } 
 }
 
 void NEOPrintData(NEOData* gpsData){
     printf("Lat: %.3f %c\nLon: %.3f %c\ndate: %i %.3f\n"
-           "Course: %.3f\nSpeed: %.3f KpH",
+           "Course: %.3f\nSpeed: %.3f KpH\n"
+           "dataStatus: %c\n",
            gpsData->latitude, gpsData->NS,
            gpsData->longitude, gpsData->EW,
            gpsData->date, gpsData->UTCTime,
-           gpsData->courseTrue, gpsData->speedKpH);
+           gpsData->courseTrue, gpsData->speedKpH,
+           gpsData->dataStatus);
+}
+
+void NEOMessageEnable(int msgClass, int msgId, int enable){
+    uint8_t msg[] = {0x06, 0x01, 0x03, 0x00, msgClass, msgId, enable};
+    NEOSendMessage(msg, 7);
 }
 
 void NEOSendMessage(uint8_t* msg, int length){
@@ -132,12 +223,8 @@ void NEOSendMessage(uint8_t* msg, int length){
         finalMsg[msgLen-2] += finalMsg[i];
         finalMsg[msgLen-1] += finalMsg[msgLen-2];
     }
-    char d;
-    for(i = 0; i < msgLen; ++i){
-        d = finalMsg[i];
-    }
+
     NEOWrite(finalMsg, msgLen);
-//    free(msg);
     free(finalMsg);
     finalMsg = NULL;
 }
